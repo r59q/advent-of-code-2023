299756f7928b12de567d9fcefc41a2eb8eb76604
r59q <andreashenriksen@yahoo.dk>
2023-12-15 22:13:35 +0100

    Cleanup

--- 2/game.h
+++ 2/game.h
@@ -1,4 +1,7 @@
 struct Game gameFromLine(char* line, int maxNoOfRounds);
+
 int getIdFromLine(char* line);
+
 struct Round* getRoundsFromLine(char* line, int maxNoOfLines);
+
 int getIndexOfColon(char* line);
